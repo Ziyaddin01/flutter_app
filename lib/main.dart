8ee71c86e9b44800b6855fe23fc8a14d2cd33d3d
Ziyaddin <ziya.huseynov2001@mail.ru>
2023-12-10 22:20:56 +0400

    Text ve stil elave etdim,Click elave eledim ve app bashliq,shriftleri deyishdim

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text('MyFlutterApp'),
-));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.deepOrangeAccent),
+      home: Scaffold(
+        appBar: AppBar(title: Text('MyFlutter App'),
+        centerTitle: true,
+        ),
+        body: Center (
+          child: Text('MyFlutter App', style: TextStyle(
+              fontSize: 20,
+              color: Colors.red,
+              fontFamily: 'GoblinOne'
+          ),)
+        ),
+          floatingActionButton: FloatingActionButton(
+          child: Text('Нажми'),onPressed: () {
+            print('Click');
+          },
+            backgroundColor: Colors.deepOrangeAccent,
+          ),
+      ),
+    );
+  }
+}
