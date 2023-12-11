6bca88eb6604242bf9577345b644b6e2fe7c29cf
Ziyaddin <ziya.huseynov2001@mail.ru>
2023-12-11 13:56:22 +0400

    Added "Picture"

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,11 @@ class MyApp extends StatelessWidget {
         centerTitle: true,
         ),
         body: Center (
-          child: OutlinedButton.icon(
-              onPressed: () {},
-              icon: Icon(Icons.adb_sharp),
-              label: Text('Settings')
+          child: Image(
+            image: AssetImage('assets/mountain.jpeg'),
+            // image: NetworkImage('https://w.forfun.com/fetch/04/04090cda03b9bcc625fa7464c954c993.jpeg'),
           )
+          // OutlinedButton.icon(onPressed: () {},icon: Icon(Icons.adb_sharp),label: Text('Settings'),)
           // ElevatedButton(onPressed: () {},child: Text('Нажми'), style: TextButton.styleFrom(backgroundColor: Colors.blueGrey),),
           // TextButton(onPressed: () {}, child: Text('Нажми на меня'), style: TextButton.styleFrom(backgroundColor: Colors.blueGrey),
           // Icon(Icons.amp_stories_outlined, size: 45, color: Colors.amber,),
